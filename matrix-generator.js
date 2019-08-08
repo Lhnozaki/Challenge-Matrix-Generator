648c235d7d11bb064a1e492ebed083824913dfc5
Lewis Nozaki <lhnozaki@gmail.com>
2019-08-07 19:20:09 -1000

    passed tests

--- matrix-generator.js
+++ matrix-generator.js
@@ -1,5 +1,23 @@
 function matrixGenerator(rows, columns) {
   // do work here
-}
 
+  let arr = [];
+
+  for (i = 0; i < rows; i++) {
+    arr.push([]);
+    for (j = 0; j < columns; j++) {
+      let random = Math.floor(Math.random() * 100) + 1;
+      arr[i].push(random);
+    }
+  }
+
+  arr.forEach(e => {
+    e.sort((a, b) => a - b);
+  });
+
+  arr.sort((a, b) => a[0] - b[0]);
+
+  return arr;
+}
+console.log(matrixGenerator(4, 4));
 module.exports = matrixGenerator;
